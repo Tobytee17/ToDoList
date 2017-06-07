51c87cbd8f323a2462186883d6b9add0ab7db712
Toby <Tobytee17@gmail.com>
2017-06-07 21:16:49 +1200

    repo update

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,12 +1,25 @@
+var listStorage = [];
+var x = 0;
+
 
 function add() {
   if($("#input").val() == "") {
     alert("Please enter a task!")
   } else {
-   $(".container").append("<div class = 'well'>" + $("#input").val());
+    //run through if statement to determine if entry already exist
+      $(".container").append("<li class = 'well'>" + $("#input").val());
+      listStorage[x] = $('#input').val();
+      x+=1;
+      console.log(listStorage);
+    }
   }
-}
 
 $(document).on("click", ".well", function(){
-  $(this).fadeOut(function () {  $(this).remove();})
+  $(this).fadeOut(750, function () {  $(this).remove();})
 });
+
+
+/*else {
+  if($('.well').html() == $('#input').val()) {
+    alert('You already have this to do!')
+  } */
