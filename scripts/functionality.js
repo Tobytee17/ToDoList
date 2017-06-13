0ed3567cdb44572530a2cd132848470ec01a0106
Tobytee17 <Tobytee17@gmail.com>
2017-06-13 22:21:54 +1200

    slight refactor and changed item appending to prepending

--- scripts/functionality.js
+++ scripts/functionality.js
@@ -1,32 +1,46 @@
 var listStorage = [];
 
 function add() {
-    var injectionValidation = /^[a-z0-9\s]+$/i;
-    if ($("#input").val() == "") {
-        alert("Please enter a task!")
+    if (emptyValidation() == false || alphaNumericalValidation() == false || duplicateValidation() == false) {
         return false;
-    }
-    //Checks for non-alphanumerical input to prevent HTML manipulation.
-    if(injectionValidation.test($("#input").val()) == false) {
-        alert("Please provide alphanumerical input only please!")
-        return false;
-    }
-    //check the storage list to see if a task has already been added
-    if ($.inArray($("#input").val().toLowerCase(), listStorage) != -1) {
-        alert("That is already on your ToDo List!")
     } else {
         listStorage.push($("#input").val().toLowerCase());
         console.log(listStorage);
         //run through if statement to determine if entry already exist
-        $(".container").append("<li class = 'well'>" + $("#input").val());
+        $(".empty").prepend("<div class = 'well'>" + $("#input").val());
         $("#input").val("");
+    }
+}
+
+//Check to see if input is blank
+function emptyValidation() {
+    if ($("#input").val() == "") {
+        alert("Please enter a task!")
+        return false;
+    }
+}
+
+//Checks for non-alphanumerical input to prevent HTML manipulation.
+function alphaNumericalValidation() {
+    var injectionValidation = /^[a-z0-9\s]+$/i;
+    if (injectionValidation.test($("#input").val()) == false) {
+        alert("Please provide alphanumerical input only please!")
+        return false;
+    }
+}
+
+//check the storage list to see if a task has already been added
+function duplicateValidation() {
+    if ($.inArray($("#input").val().toLowerCase(), listStorage) != -1) {
+        alert("That is already on your ToDo List!")
+        return false;
     }
 }
 
 //allows for the deletion of a specific div when it is clicked on
 $(document).on("click", ".well", function() {
     for (var x in listStorage) {
-        if (listStorage[x] == $(this).html()) {
+        if (listStorage[x] == $(this).html().toLowerCase()) {
             listStorage.splice(x, 1);
         }
     }
